67a3e23ffd0760678aeb4f969d0040dc8383ccc1
MatheusBLopes <matheusblopes10@gmail.com>
2021-11-16 17:44:03 -0300

    Updates admin

--- psique/apps/core/admin.py
+++ psique/apps/core/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 
 from .models import Category, Psychologist
 
-admin.site.register(Category)
-
 
 class PsiqueAdmin(admin.ModelAdmin):
     fields = [
@@ -24,7 +22,14 @@ class PsiqueAdmin(admin.ModelAdmin):
         "registration_approved_by",
     ]
     list_display = ("full_name", "crp", "category", "whatsapp_phone", "blocked", "in_an_appointment")
-    # readonly_fields = ("original_error",)
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    fields = [
+        "name",
+    ]
+    list_display = "name"
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Psychologist, PsiqueAdmin)
