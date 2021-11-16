7b6cd78769969c6fc769d02376562acbfe5230d6
Matheus Bachiste Lopes <matheus.bachiste@olist.com>
2021-11-16 16:06:29 -0300

    Creates endpoints

--- psique/apps/core/routes.py
+++ psique/apps/core/routes.py
@@ -1,15 +1,36 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
 
-from apps.core.api import PsycologistViewSet, RegisterView, LoginView, UserView, LogoutView
-
+from apps.core.api import (
+    AcceptConsultationView,
+    CategoryViewSet,
+    CloseConsultationView,
+    LoginView,
+    LogoutAndBlacklistRefreshTokenForUserView,
+    PsychologistViewSet,
+    RegisterView,
+    RequestConsultationView,
+)
 
 app_name = "core"
 
+router = DefaultRouter()
+router.register("psychologist", PsychologistViewSet)
+router.register("category", CategoryViewSet)
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
-    path('user', UserView.as_view()),
-    path('logout', LogoutView.as_view()),
-    path('psycologist', PsycologistViewSet.as_view({"get":"list"}), name="psycologist")
-    ]
+    path("register/", RegisterView.as_view(), name="create_user"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("login/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
+    path("logout/", LogoutAndBlacklistRefreshTokenForUserView.as_view(), name="blacklist"),
+    path("request-consultation/", RequestConsultationView.as_view(), name="request-consultation"),
+    path(
+        r"accept-consultation/<uuid:psychologist_id>/",
+        AcceptConsultationView.as_view(),
+        name="accept-consultation",
+    ),
+    path("end-consultation/", CloseConsultationView.as_view(), name="free-for-consultation"),
+]
+
+urlpatterns += router.urls
