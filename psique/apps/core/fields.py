78910a7327cad8c1b8fb662780ba2b62719e45ab
Matheus Bachiste Lopes <matheus.bachiste@olist.com>
2021-11-16 16:03:56 -0300

    Creates uuid field

---
+++ psique/apps/core/fields.py
@@ -0,0 +1,20 @@
+import uuid
+
+from django.db import models
+
+
+class UUIDPrimaryKeyField(models.UUIDField):
+    def __init__(self, *args, **kwargs):
+        kwargs["primary_key"] = True
+        kwargs["unique"] = True
+        kwargs["editable"] = False
+        super().__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        value = super().pre_save(model_instance, add)
+
+        if value is None:
+            value = uuid.uuid4()
+            setattr(model_instance, self.attname, value)
+
+        return value
