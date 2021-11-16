8a9b3c8546a7c014abc3edfed1f9d8bbdfc1c593
MatheusBLopes <matheusblopes10@gmail.com>
2021-11-16 17:35:53 -0300

    Adds categories to admin

--- psique/apps/core/admin.py
+++ psique/apps/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Psychologist
+from .models import Category, Psychologist
+
+admin.site.register(Category)
 
 
 class PsiqueAdmin(admin.ModelAdmin):
